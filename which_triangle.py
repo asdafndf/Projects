0aefa4b1f695bab3e4b2275c035ee248f751afe5
asdafndf <109662927+asdafndf@users.noreply.github.com>
2022-07-20 20:44:00 +0200

    Create which_triangle.py

---
+++ which_triangle.py
@@ -0,0 +1,12 @@
+#Chose the length of a triangles 3 sides. The code tells you whether it is equilateral, isosceles, scalene or no triangle at all.
+a = 6
+b = 6
+c = 6
+if a==b==c:
+    print("equilateral triangle")
+elif a==b or a==c or b==c:
+    print("isosceles triangle")
+elif a+b>c and a+c>b and b+c>a:
+    print("scalene triangle")
+else:
+    print("no such triangle exists")
