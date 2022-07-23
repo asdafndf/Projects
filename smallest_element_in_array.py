e3ae84affb72cfc355a7b288ed0cd98ee0c62c91
asdafndf <109662927+asdafndf@users.noreply.github.com>
2022-07-23 11:54:30 +0200

    minimum number in array

---
+++ smallest_element_in_array.py
@@ -0,0 +1,11 @@
+# %%
+arr = [25, 11, 2, 75, 56]
+def minimum_number(arr):
+    min = arr[0]
+    for x in range(0, len(arr)):
+        if(arr[x] < min):
+            min = arr[x]
+    return("Smallest element present in given array: %s" % (min))
+minimum_number(arr)
+
+
