eb8f446b387b4fcb59cbb169007c9194824b45e3
asdafndf <109662927+asdafndf@users.noreply.github.com>
2022-07-25 23:05:55 +0200

    Update Wilson_prime.py

--- Wilson_prime.py
+++ Wilson_prime.py
@@ -1,13 +1,11 @@
-# In theory this code determines whether n is a Wilson prime or not, but it has to be tested.
+# This code determines whether a number is Wilson prime or not. 
 
 from math import *
 def am_i_wilson(n):
-    return (factorial(n-1)+1) % pow(n,2) == 0 
-
-def am_i_wilson(n):    
-    def factorial(n):
-        product = 1
-        for x in range(1,n+1):
-            product = product * x
-        return product
-    return (factorial(n-1)+1) % n*n == 0
+    for x in range(2,ceil(sqrt(n))):
+        if n % x == 0:
+            return False
+    if (n-1) <= 0:
+        return False
+    else:
+        return (factorial(n-1)+1) % (n*n) == 0
