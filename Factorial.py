93080557606d9e8ae444908cb64a14512f75a7e0
asdafndf <109662927+asdafndf@users.noreply.github.com>
2022-07-25 23:19:06 +0200

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,6 +1,6 @@
 # This code gives you n!. For negative numbers it doesn't work
 
-def factorial(n):
+def factor(n):
     product = 1
     if n < 0:
         return "%s is negative, sorry" % (n)
@@ -8,3 +8,9 @@ def factorial(n):
         for x in range(1,n+1):
             product = product * x
     return "%s! equals %s" % (n,product)
+
+# You can also import:
+from math import *
+def factor2(n):
+    return factorial(n)
+factor2(4)
