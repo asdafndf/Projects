96222bc604e319b7b1c59a04e80e3dc823cdd224
asdafndf <109662927+asdafndf@users.noreply.github.com>
2022-07-26 11:32:39 +0200

    Create mad_libs.py

---
+++ mad_libs.py
@@ -0,0 +1,11 @@
+# Simple, not-so-funny Mad Libs game
+noun1 = input("Enter a noun: ")
+plural_noun = input("Now a plural noun: ")
+noun2 = input("Enter another noun (singular): ")
+noun3 = input("Enter one more noun (singular): ")
+
+
+print("Twinkle, twinkle, little %s," % (noun1))
+print("How I wonder what %s are!" % (plural_noun))
+print("Up above the %s so high," % (noun2))
+print("Like a %s in the sky." % (noun3))
