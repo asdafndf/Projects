3df4abeb272b94a927aee65d01ab72917919ae80
asdafndf <109662927+asdafndf@users.noreply.github.com>
2022-07-26 12:23:02 +0200

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -1,7 +1,7 @@
 # In theory this code determines whether n is prime or not, however it has to be checked
 from math import *
 def is_prime(n):
-    for x in range(2,ceil(sqrt(n))):
+    for x in range(2,int(sqrt(n))+1):
         if n % x == 0:
             return "%s is not a prime" % (n)
         else:
