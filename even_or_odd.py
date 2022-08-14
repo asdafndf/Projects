8d5331e13ebdbda0769277083d95fd3b4617770c
asdafndf <109662927+asdafndf@users.noreply.github.com>
2022-08-14 12:38:20 +0200

    Update even_or_odd.py

--- even_or_odd.py
+++ even_or_odd.py
@@ -1,6 +1,7 @@
-# Write a number after x. The code will tell whether it is an even or an odd number.
-x = 6
-if x % 2 == 0:
-    print("even number")
-elif x % 2 == 1:
-    print("odd number")
+# The code will tell you whether x is an even or an odd number.
+
+def even_or_odd(x):
+    if x % 2 == 0:
+        print("even number")
+    elif x % 2 == 1:
+        print("odd number")
