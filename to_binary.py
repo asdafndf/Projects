ec93e334e8f63e766ee68a97edef06b95b70bb78
asdafndf <109662927+asdafndf@users.noreply.github.com>
2022-08-19 22:10:11 +0200

    Create to_binary.py

---
+++ to_binary.py
@@ -0,0 +1,16 @@
+# Convert a decimal number to binary. The first one is my solution
+
+from math import *
+
+def to_binary(n):
+    list_ = []
+    while n > 0:
+        list_.append(n % 2)
+        n = floor(n/2)
+    to_string = [str(x) for x in list_]
+    return int("".join(to_string[::-1]))
+
+# Probably the best practice:
+
+def to_binary(n):
+    return int(f"{n:b}")
