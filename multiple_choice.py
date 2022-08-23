fa008e51ad9e3ef0e8487ee30c0be0cae3be1a13
asdafndf <109662927+asdafndf@users.noreply.github.com>
2022-08-23 14:58:58 +0200

    Update multiple_choice.py

--- multiple_choice.py
+++ multiple_choice.py
@@ -6,15 +6,21 @@ class Question:
         self.answer = answer
 
 question_prompts = [
-    "What color are apples?\n (a) Red/Green\n(b) Purple\n(c) Orange\n\n",
-    "What color are bananas?\n (a) Teal\n(b) Yellow\n(c) Magenta\n\n",
-    "What color are strawberries?\n (a) Yellow\n(b) Red\n(c) Blue\n\n"
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What color are bananas?\n(a) Teal\n(b) Yellow\n(c) Magenta\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
+
+question_answers = [
+    "a",
+    "b",
+    "b"
 ]
 
 questions = [
-    Question(question_prompts[0], "a"),
-    Question(question_prompts[1], "b"),
-    Question(question_prompts[2], "b")
+    Question(question_prompts[0], question_answers[0]),
+    Question(question_prompts[1], question_answers[1]),
+    Question(question_prompts[2], question_answers[2])
 ]
 
 def run_test(questions):
