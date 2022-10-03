794801c8040ece9d852ee60b9f3e90a731061a75
asdafndf <109662927+asdafndf@users.noreply.github.com>
2022-10-03 23:32:53 +0200

    Create enumerate.py

---
+++ enumerate.py
@@ -0,0 +1,28 @@
+# How does enumerate() work?
+
+values = ["a", "b", "c"]
+for value in values:
+    print(value)
+
+values = ["a", "b", "c"]
+index = 0
+for value in values:
+    print(index, value)
+    index += 1
+
+values = ["a", "b", "c"]
+for index in range(len(values)):
+    value = values[index]
+    print(value)
+
+
+values = ["a", "b", "c"]
+for index, value in enumerate(values):
+    print(index, value)
+
+for x, y in [[0,"a"], [1, "b"], [3, "c"]]:
+    print(x,y)
+
+
+
+
