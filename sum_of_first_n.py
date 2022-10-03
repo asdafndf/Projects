68051bf1f16eff2eeb8f84d7b3db90eba1b75282
asdafndf <109662927+asdafndf@users.noreply.github.com>
2022-10-03 15:37:17 +0200

    Update sum_of_first_n.py

--- sum_of_first_n.py
+++ sum_of_first_n.py
@@ -3,7 +3,7 @@ def summation(n):
     sum = 0
     for x in range(1, n + 1):
         sum += x
-    print(f"Sum of first {n} numbers is: {sum}"
+    return(f"Sum of first {n} numbers is: {sum}"
 # Another options 1:
 def summation(n):
     return sum(range(1, n+1))
