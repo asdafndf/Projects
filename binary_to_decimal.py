4f5a1872eaa44fef09274a7012810b4b6b87a4b7
asdafndf <109662927+asdafndf@users.noreply.github.com>
2022-11-04 09:04:24 +0100

    Update binary_to_decimal.py

--- binary_to_decimal.py
+++ binary_to_decimal.py
@@ -1,11 +1,11 @@
 # Turn a binary number to decimal
 
-def binary_to_decimal(n):
+def method1(n):
     arr = [int(x) for x in str(n)]
     result = 0
     for x in arr:
         result = result * 2 + x
     return result
 
-def binary_to_decimal2(n):
+def method2(n):
     return int(str(n), 2)
